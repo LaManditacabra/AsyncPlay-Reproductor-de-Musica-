30d24ee56ec595f2e3399c80d2bfc5e4b9fed7e2
LaManditacabra <LaManditacabra@users.noreply.github.com>
2026-08-19 09:45:46 +0000

    Version 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.example.musicplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.3.0"
 
         // Configuración del repo de GitHub usado por el sistema de actualizaciones.
         buildConfigField("String", "REPO_OWNER", "\"LaManditacabra\"")
